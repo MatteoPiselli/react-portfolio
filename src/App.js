513bc27707807946034f1c8ff1e64daf7dc73024
MatteoPiselli <pro.matteo.piselli@gmail.com>
2024-09-27 14:32:06 +0200

    code source

--- src/App.js
+++ src/App.js
@@ -1,23 +1,11 @@
-import logo from './logo.svg';
 import './App.css';
+import header from './header.png'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <img src={header} alt="header" />
+      <h1>Excepteur duis non aliquip incididunt esse. Anim aliquip irure commodo labore irure cillum elit et elit deserunt enim. Veniam velit tempor irure minim. Quis ipsum aliquip irure eiusmod exercitation id quis non laboris esse reprehenderit nisi reprehenderit esse. Eiusmod sunt ut minim reprehenderit nulla exercitation velit cupidatat quis voluptate eiusmod eu anim cupidatat. Nulla amet eu excepteur deserunt.</h1>
     </div>
   );
 }
