35243b32365ef00803bc74ffeaa13d89f471d984
MatteoPiselli <pro.matteo.piselli@gmail.com>
2024-09-27 22:47:09 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import React, { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import './styles/index.css'; // Importer le fichier CSS de Tailwind
 import App from './App';
-import AppTest from './AppTest';
 
 const root = createRoot(document.getElementById('root'));
 
