f5dc2902e9d00cd6f9d38009981a5d28a979d34c
Matteo Piselli <pro.matteo.piselli@gmail.com>
2024-10-01 22:28:03 +0200

    Create copyright.js

---
+++ src/copyright.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Copyright() {
+  return (
+    <footer className="w-full text-center font-bold">
+      <p className="text-sm">
+        Copyright &copy; {new Date().getFullYear()} Matteo Piselli. Tous droits réservés.
+      </p>
+    </footer>
+  );
+}
+
+export default Copyright;
