8a550f2380879d819915722bab7e3c5dd24e113b
MatteoPiselli <pro.matteo.piselli@gmail.com>
2024-11-13 02:08:02 +0100

    Update competences.js

--- src/competences.js
+++ src/competences.js
@@ -40,122 +40,176 @@ const Competences = () => {
       <h1 className="text-center text-3xl mb-12">Mes Compétences</h1>
 
       <div className="ml-[10%] mr-[10%] grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 gap-8">
-        <div className="relative bg-gray-800 rounded-lg p-5">
+        <div className="relative transform transition-transform duration-300 hover:scale-105 bg-gray-800 rounded-lg p-5">
           <img src={html} alt="html" className="rounded-lg h-auto" />
           <div className="absolute top-0 right-0 m-2 bg-black bg-opacity-50 rounded-lg px-2 py-1">
             <span className="text-white">Front-End</span>
           </div>
-        </div>
-
-        <div className="relative bg-gray-800 rounded-lg p-5">
-          <img src={css} alt="css" />
-          <div className="absolute top-0 right-0 m-2 bg-black bg-opacity-50 rounded-lg px-2 py-1">
-            <span className="text-white">Front-End</span>
+          <div className="absolute inset-0 bg-black bg-opacity-75 flex items-center justify-center rounded-lg opacity-0 hover:opacity-100 transition-opacity duration-300">
+            <span className="text-sm text-white">HTML5</span>
           </div>
         </div>
 
-        <div className="relative bg-gray-800 rounded-lg p-5">
+        <div className="relative transform transition-transform duration-300 hover:scale-105 bg-gray-800 rounded-lg p-5">
+          <img src={css} alt="css" className="rounded-lg" />
+
+          <div className="absolute top-0 right-0 m-2 bg-black bg-opacity-50 rounded-lg px-2 py-1">
+            <span className="text-white">Front-End</span>
+          </div>
+
+          {/* Superposition centrale au survol */}
+          <div className="absolute inset-0 bg-black bg-opacity-75 flex items-center justify-center rounded-lg opacity-0 hover:opacity-100 transition-opacity duration-300">
+            <span className="text-sm text-white">CSS3</span>
+          </div>
+        </div>
+
+        <div className="relative transform transition-transform duration-300 hover:scale-105 bg-gray-800 rounded-lg p-5">
           <img src={js} alt="js" />
           <div className="absolute top-0 right-0 m-2 bg-black bg-opacity-50 rounded-lg px-2 py-1">
             <span className="text-white">Front-End</span>
           </div>
+          <div className="absolute inset-0 bg-black bg-opacity-75 flex items-center justify-center rounded-lg opacity-0 hover:opacity-100 transition-opacity duration-300">
+            <span className="text-sm text-white">JAVASCRIPT</span>
+          </div>
         </div>
 
-        <div className="relative bg-gray-800 rounded-lg p-5">
+        <div className="relative transform transition-transform duration-300 hover:scale-105 bg-gray-800 rounded-lg p-5">
           <img src={react} alt="react" />
           <div className="absolute top-0 right-0 m-2 bg-black bg-opacity-50 rounded-lg px-2 py-1">
             <span className="text-white">Front-End</span>
           </div>
+          <div className="absolute inset-0 bg-black bg-opacity-75 flex items-center justify-center rounded-lg opacity-0 hover:opacity-100 transition-opacity duration-300">
+            <span className="text-sm text-white">REACT</span>
+          </div>
         </div>
 
-        <div className="relative bg-gray-800 rounded-lg p-5">
+        <div className="relative transform transition-transform duration-300 hover:scale-105 bg-gray-800 rounded-lg p-5">
           <img src={tailwind} alt="tailwind css" />
           <div className="absolute top-0 right-0 m-2 bg-black bg-opacity-50 rounded-lg px-2 py-1">
             <span className="text-white">Front-End</span>
           </div>
+          <div className="absolute inset-0 bg-black bg-opacity-75 flex items-center justify-center rounded-lg opacity-0 hover:opacity-100 transition-opacity duration-300">
+            <span className="text-sm text-white">TAILWIND</span>
+          </div>
         </div>
 
-        <div className="relative bg-gray-800 rounded-lg p-5">
+        <div className="relative transform transition-transform duration-300 hover:scale-105 bg-gray-800 rounded-lg p-5">
           <img src={php} alt="php" />
           <div className="absolute top-0 right-0 m-2 bg-black bg-opacity-50 rounded-lg px-2 py-1">
             <span className="text-white">Back-End</span>
           </div>
+          <div className="absolute inset-0 bg-black bg-opacity-75 flex items-center justify-center rounded-lg opacity-0 hover:opacity-100 transition-opacity duration-300">
+            <span className="text-sm text-white">PHP</span>
+          </div>
         </div>
 
-        <div className="relative bg-gray-800 rounded-lg p-5">
+        <div className="relative transform transition-transform duration-300 hover:scale-105 bg-gray-800 rounded-lg p-5">
           <img src={java} alt="java" />
           <div className="absolute top-0 right-0 m-2 bg-black bg-opacity-50 rounded-lg px-2 py-1">
             <span className="text-white">Back-End</span>
           </div>
+          <div className="absolute inset-0 bg-black bg-opacity-75 flex items-center justify-center rounded-lg opacity-0 hover:opacity-100 transition-opacity duration-300">
+            <span className="text-sm text-white">JAVA</span>
+          </div>
         </div>
 
-        <div className="relative bg-gray-800 rounded-lg p-5">
+        <div className="relative transform transition-transform duration-300 hover:scale-105 bg-gray-800 rounded-lg p-5">
           <img src={mySQL} alt="MySQL" />
           <div className="absolute top-0 right-0 m-2 bg-black bg-opacity-50 rounded-lg px-2 py-1">
             <span className="text-white">Base de Donées</span>
           </div>
+          <div className="absolute inset-0 bg-black bg-opacity-75 flex items-center justify-center rounded-lg opacity-0 hover:opacity-100 transition-opacity duration-300">
+            <span className="text-sm text-white">MYSQL</span>
+          </div>
         </div>
 
-        <div className="relative bg-gray-800 rounded-lg p-5">
+        <div className="relative transform transition-transform duration-300 hover:scale-105 bg-gray-800 rounded-lg p-5">
           <img src={python} alt="python" />
           <div className="absolute top-0 right-0 m-2 bg-black bg-opacity-50 rounded-lg px-2 py-1">
             <span className="text-white">Algo</span>
           </div>
+          <div className="absolute inset-0 bg-black bg-opacity-75 flex items-center justify-center rounded-lg opacity-0 hover:opacity-100 transition-opacity duration-300">
+            <span className="text-sm text-white">PYTHON</span>
+          </div>
         </div>
 
-        <div className="relative bg-gray-800 rounded-lg p-5">
+        <div className="relative transform transition-transform duration-300 hover:scale-105 bg-gray-800 rounded-lg p-5">
           <img src={bash} alt="bash" />
           <div className="absolute top-0 right-0 m-2 bg-black bg-opacity-50 rounded-lg px-2 py-1">
             <span className="text-white">Script</span>
           </div>
+          <div className="absolute inset-0 bg-black bg-opacity-75 flex items-center justify-center rounded-lg opacity-0 hover:opacity-100 transition-opacity duration-300">
+            <span className="text-sm text-white">BASH</span>
+          </div>
         </div>
 
-        <div className="relative bg-gray-800 rounded-lg p-5">
+        <div className="relative transform transition-transform duration-300 hover:scale-105 bg-gray-800 rounded-lg p-5">
           <img src={uml} alt="uml" />
           <div className="absolute top-0 right-0 m-2 bg-black bg-opacity-50 rounded-lg px-2 py-1">
             <span className="text-white">Modélisation</span>
           </div>
+          <div className="absolute inset-0 bg-black bg-opacity-75 flex items-center justify-center rounded-lg opacity-0 hover:opacity-100 transition-opacity duration-300">
+            <span className="text-sm text-white">UML</span>
+          </div>
         </div>
 
-        <div className="relative bg-gray-800 rounded-lg p-5">
+        <div className="relative transform transition-transform duration-300 hover:scale-105 bg-gray-800 rounded-lg p-5">
           <img src={github} alt="github" />
           <div className="absolute top-0 right-0 m-2 bg-black bg-opacity-50 rounded-lg px-2 py-1">
             <span className="text-white">Outils Dév</span>
           </div>
+          <div className="absolute inset-0 bg-black bg-opacity-75 flex items-center justify-center rounded-lg opacity-0 hover:opacity-100 transition-opacity duration-300">
+            <span className="text-sm text-white">GITHUB</span>
+          </div>
         </div>
 
-        <div className="relative bg-gray-800 rounded-lg p-5">
+        <div className="relative transform transition-transform duration-300 hover:scale-105 bg-gray-800 rounded-lg p-5">
           <img src={figma} alt="figma" />
           <div className="absolute top-0 right-0 m-2 bg-black bg-opacity-50 rounded-lg px-2 py-1">
             <span className="text-white">UX/UI Design</span>
           </div>
+          <div className="absolute inset-0 bg-black bg-opacity-75 flex items-center justify-center rounded-lg opacity-0 hover:opacity-100 transition-opacity duration-300">
+            <span className="text-sm text-white">FIGMA</span>
+          </div>
         </div>
 
-        <div className="relative bg-gray-800 rounded-lg p-5">
+        <div className="relative transform transition-transform duration-300 hover:scale-105 bg-gray-800 rounded-lg p-5">
           <img src={vscode} alt="vscode" />
           <div className="absolute top-0 right-0 m-2 bg-black bg-opacity-50 rounded-lg px-2 py-1">
             <span className="text-white">IDE</span>
           </div>
+          <div className="absolute inset-0 bg-black bg-opacity-75 flex items-center justify-center rounded-lg opacity-0 hover:opacity-100 transition-opacity duration-300">
+            <span className="text-sm text-white">VSCODE</span>
+          </div>
         </div>
 
-        <div className="relative bg-gray-800 rounded-lg p-5">
+        <div className="relative transform transition-transform duration-300 hover:scale-105 bg-gray-800 rounded-lg p-5">
           <img src={windows} alt="windows" />
           <div className="absolute top-0 right-0 m-2 bg-black bg-opacity-50 rounded-lg px-2 py-1">
             <span className="text-white">OS</span>
           </div>
+          <div className="absolute inset-0 bg-black bg-opacity-75 flex items-center justify-center rounded-lg opacity-0 hover:opacity-100 transition-opacity duration-300">
+            <span className="text-sm text-white">WINDOWS</span>
+          </div>
         </div>
 
-        <div className="relative bg-gray-800 rounded-lg p-5">
+        <div className="relative transform transition-transform duration-300 hover:scale-105 bg-gray-800 rounded-lg p-5">
           <img src={ubuntu} alt="ubuntu" />
           <div className="absolute top-0 right-0 m-2 bg-black bg-opacity-50 rounded-lg px-2 py-1">
             <span className="text-white">OS</span>
           </div>
+          <div className="absolute inset-0 bg-black bg-opacity-75 flex items-center justify-center rounded-lg opacity-0 hover:opacity-100 transition-opacity duration-300">
+            <span className="text-sm text-white">UBUNTU</span>
+          </div>
         </div>
 
-        <div className="relative bg-gray-800 rounded-lg p-5">
+        <div className="relative transform transition-transform duration-300 hover:scale-105 bg-gray-800 rounded-lg p-5">
           <img src={macos} alt="macOS" />
           <div className="absolute top-0 right-0 m-2 bg-black bg-opacity-50 rounded-lg px-2 py-1">
             <span className="text-white">OS</span>
+          </div>
+          <div className="absolute inset-0 bg-black bg-opacity-75 flex items-center justify-center rounded-lg opacity-0 hover:opacity-100 transition-opacity duration-300">
+            <span className="text-sm text-white">MACOS</span>
           </div>
         </div>
       </div>
