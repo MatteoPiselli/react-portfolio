0dafd058c99078e5137072794f771d598df7f747
MatteoPiselli <pro.matteo.piselli@gmail.com>
2024-11-21 20:01:09 +0100

    Update DropdownMenu.js

--- src/components/DropdownMenu.js
+++ src/components/DropdownMenu.js
@@ -2,8 +2,7 @@ import React, { useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import { motion, AnimatePresence } from "framer-motion";
 import Copyright from "./copyright";
-
-// Images
+//images
 import Home from "../assets/images/icons/accueil.png";
 import Code from "../assets/images/icons/code.png";
 import Stage from "../assets/images/icons/stage.png";
@@ -18,81 +17,70 @@ import Formation from "../assets/images/icons/formation.png";
 
 const DropdownMenu = () => {
   const [isOn, setIsOn] = useState(false); // État pour gérer l'affichage du menu
+
   const toggleMenu = () => setIsOn(!isOn); // Fonction pour basculer l'affichage du menu
 
   const spring = {
     type: "spring",
-    stiffness: 300,
-    damping: 20,
+    stiffness: 300, // Réduit la raideur pour un mouvement plus fluide
+    damping: 20, // Amortit le mouvement pour le rendre plus doux
   };
+
+  const [isAnimating, setIsAnimating] = useState(false);
 
   const navigate = useNavigate();
 
   const handleNavigation = (e, path, anchor) => {
     e.preventDefault(); // Empêcher la redirection immédiate
+    setIsAnimating(true); // Déclencher l'animation
+
     setTimeout(() => {
-      navigate(path, { state: { scrollTo: anchor } });
-    }, 500);
+      navigate(path, { state: { scrollTo: anchor } }); // Transmettre l'ancre en état
+    }, 500); // Délai correspondant à la durée de l'animation
   };
 
   return (
     <div>
-      {/* Bouton menu hamburger / croix */}
+      {/* Bouton qui déclenche le menu déroulant */}
       <div
-        className="absolute top-8 left-8 w-10 h-10 flex items-center justify-center cursor-pointer z-50" // Ajout du z-index ici
+        className={`absolute top-8 right-8 w-20 h-10 flex items-center z-20
+        ${isOn ? "justify-end" : "justify-start"} + ${
+          isOn ? "bg-sky-400" : "bg-gray-600"
+        }        
+      rounded-full p-2 cursor-pointer`}
         onClick={toggleMenu}
       >
-        {/* Animation dynamique du menu hamburger / croix */}
+        {/* La poignée du menu, animée avec Framer Motion */}
         <motion.div
-          initial={false}
-          animate={isOn ? "open" : "closed"}
-          className="relative w-8 h-8"
-        >
-          <motion.span
-            className="absolute top-1 left-0 w-8 h-1 bg-white rounded"
-            variants={{
-              closed: { rotate: 0, y: 0 },
-              open: { rotate: 45, y: 7 },
-            }}
-            transition={spring}
-          />
-          <motion.span
-            className="absolute top-3 left-0 w-8 h-1 bg-white rounded"
-            variants={{
-              closed: { opacity: 1 },
-              open: { opacity: 0 },
-            }}
-            transition={spring}
-          />
-          <motion.span
-            className="absolute top-5 left-0 w-8 h-1 bg-white rounded"
-            variants={{
-              closed: { rotate: 0, y: 0 },
-              open: { rotate: -45, y: -7 },
-            }}
-            transition={spring}
-          />
-        </motion.div>
+          className="bg-white w-10 h-10 rounded-full shadow-md" // Taille et style de la poignée
+          layout
+          transition={spring} // Animation fluide
+        />
       </div>
 
       <AnimatePresence>
-        {/* Menu déroulant */}
+        {/* Menu déroulant qui s'affiche ou non en fonction de l'état `isOn` */}
         {isOn && (
           <motion.div
-            className="absolute top-0 left-0 bg-white bg-opacity-75 w-full md:w-auto h-full rounded-md p-4 z-40" // Ajout du z-index ici pour que le menu soit en dessous du bouton
-            initial={{ opacity: 0, scale: 0 }}
-            animate={{ opacity: 1, scale: 1 }}
-            exit={{ opacity: 0, scale: 0 }}
-            transition={spring}
+            className="absolute top-0 bg-white bg-opacity-75 w-full md:w-auto h-full rounded-md p-4 z-10"
+            initial={{ x: "-100%" }} // Le menu commence hors de l'écran à gauche
+            animate={{ x: "0%" }} // Le menu glisse vers la position finale
+            exit={{ x: "-100%" }} // Le menu glisse hors de l'écran quand il disparaît
+            transition={spring} // Transition avec effet de ressort
           >
             <p className="font-bold text-xl md:text-3xl">Matteo PISELLI</p>
-            <p className="italic text-md md:text-lg">Etudiant Développeur Web</p>
-            <br />
+            <p className="italic text-md md:text-lg">
+              Etudiant Développeur Web
+            </p>
+            <div className="h-20 md:h-8" />
             <ul className="flex flex-col items-center justify-center md:items-start space-y-4">
               <li>
                 <div className="flex space-x-4">
                   <img src={Home} alt="accueil" className="w-[25px] h-[25px]" />
-                  <Link to="/react-portfolio/" className="text-gray-800 hover:text-sky-600">
+                  <Link
+                    to="/react-portfolio/"
+                    className="text-gray-800 hover:text-sky-600 transform transition-transform duration-300 hover:scale-105"
+                  >
                     Accueil
                   </Link>
                 </div>
@@ -107,7 +95,7 @@ const DropdownMenu = () => {
                   <Link
                     href="/aboutMe"
                     onClick={(e) => handleNavigation(e, "/aboutMe", "#aboutMe")}
-                    className="text-gray-800 hover:text-sky-600"
+                    className="text-gray-800 hover:text-sky-600 transform transition-transform duration-300 hover:scale-105"
                   >
                     À propos
                   </Link>
@@ -125,7 +113,7 @@ const DropdownMenu = () => {
                     onClick={(e) =>
                       handleNavigation(e, "/competences", "#competences")
                     }
-                    className="text-gray-800 hover:text-sky-600"
+                    className="text-gray-800 hover:text-sky-600 transform transition-transform duration-300 hover:scale-105"
                   >
                     Compétences Dév. & OS
                   </Link>
@@ -143,7 +131,7 @@ const DropdownMenu = () => {
                     onClick={(e) =>
                       handleNavigation(e, "/experiences", "#experiences")
                     }
-                    className="text-gray-800 hover:text-sky-600"
+                    className="text-gray-800 hover:text-sky-600 transform transition-transform duration-300 hover:scale-105"
                   >
                     Expériences professionnelles
                   </Link>
@@ -159,7 +147,7 @@ const DropdownMenu = () => {
                   <Link
                     to="/projets"
                     onClick={(e) => handleNavigation(e, "/projets", "#projets")}
-                    className="text-gray-800 hover:text-sky-600"
+                    className="text-gray-800 hover:text-sky-600 transform transition-transform duration-300 hover:scale-105"
                   >
                     Mes projets
                   </Link>
@@ -177,12 +165,13 @@ const DropdownMenu = () => {
                     onClick={(e) =>
                       handleNavigation(e, "/formation", "#formation")
                     }
-                    className="text-gray-800 hover:text-sky-600"
+                    className="text-gray-800 hover:text-sky-600 transform transition-transform duration-300 hover:scale-105"
                   >
                     Formation
                   </Link>
                 </div>
               </li>
+
               <li>
                 <div className="flex space-x-4">
                   <img
@@ -195,7 +184,7 @@ const DropdownMenu = () => {
                     onClick={(e) =>
                       handleNavigation(e, "/certifications", "#certifications")
                     }
-                    className="text-gray-800 hover:text-sky-600"
+                    className="text-gray-800 hover:text-sky-600 transform transition-transform duration-300 hover:scale-105"
                   >
                     Certifications
                   </Link>
@@ -210,7 +199,7 @@ const DropdownMenu = () => {
                   />
                   <a
                     href="https://github.com/MatteoPiselli"
-                    className="text-gray-800 hover:text-sky-600"
+                    className="text-gray-800 hover:text-sky-600 transform transition-transform duration-300 hover:scale-105"
                   >
                     Github
                   </a>
@@ -225,17 +214,17 @@ const DropdownMenu = () => {
                   />
                   <a
                     href="https://www.linkedin.com/in/matteo-piselli-354a27231/"
-                    className="text-gray-800 hover:text-sky-600"
+                    className="text-gray-800 hover:text-sky-600 transform transition-transform duration-300 hover:scale-105"
                   >
                     Linkedin
                   </a>
                 </div>
               </li>
             </ul>
-            <div className="absolute bottom-48 left-1/2 transform -translate-x-1/2">
+            <div className="absolute mt-20 md:bottom-48 left-1/2 transform -translate-x-1/2">
               CONTACT
             </div>
-            <div className="absolute bottom-5 flex flex-col space-y-4">
+            <div className="flex flex-col items-center md:items-start justify-center space-y-4 mt-28 md:bottom-5">
               <div className="flex space-x-2">
                 <img
                   src={Telephone}
@@ -247,7 +236,12 @@ const DropdownMenu = () => {
 
               <div className="flex space-x-2">
                 <img src={Mail} alt="mail" className="w-[25px] h-[25px]" />
-                <a href="mailto:pro.matteo.piselli@gmail.com" className="hover:text-sky-600">pro.matteo.piselli@gmail.com</a>
+                <a
+                  href="mailto:pro.matteo.piselli@gmail.com"
+                  className="hover:text-sky-600 transform transition-transform duration-300 hover:scale-105"
+                >
+                  pro.matteo.piselli@gmail.com
+                </a>
               </div>
 
               <Copyright />
