752f5cd637e5a9a49f43aaa1ca5d490f81c250b8
MatteoPiselli <pro.matteo.piselli@gmail.com>
2025-02-08 21:33:23 +0100

    🐛 fix bug dependencies

--- src/index.js
+++ src/index.js
@@ -1,14 +1,12 @@
-import React, { StrictMode } from 'react';
-import { createRoot } from 'react-dom/client';
-import './styles/index.css'; // Importer le fichier CSS de Tailwind
-import App from './App';
-import AppTest from './AppTest'
+import React, { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./styles/index.css";
+import App from "./App";
 
-const root = createRoot(document.getElementById('root'));
+const root = createRoot(document.getElementById("root"));
 
 root.render(
   <StrictMode>
     <App />
   </StrictMode>
 );
-
