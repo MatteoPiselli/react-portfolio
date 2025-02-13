63a60b8cd9bf8703c5a0a0a7ed095e45d4ec00ac
MatteoPiselli <pro.matteo.piselli@gmail.com>
2025-02-13 19:22:45 +0100

    🏗️ Ajout de BrowserRouter pour la gestion des routes avec un basename dynamique

--- src/index.js
+++ src/index.js
@@ -1,12 +1,16 @@
 import React, { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
 import "./styles/index.css";
 import App from "./App";
+
+const basename =
+  process.env.NODE_ENV === "production" ? "/react-portfolio" : "/";
 
 const root = createRoot(document.getElementById("root"));
 
 root.render(
-  <StrictMode>
+  <BrowserRouter basename={basename}>
     <App />
-  </StrictMode>
+  </BrowserRouter>
 );
