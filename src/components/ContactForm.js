4a7424a63de826e150ebf35ee2b63e883b88fc73
MatteoPiselli <pro.matteo.piselli@gmail.com>
2025-02-13 02:45:25 +0100

    Correction de la variable d'environnement pour l'ID de modèle EmailJS dans le formulaire de contact

--- src/components/ContactForm.js
+++ src/components/ContactForm.js
@@ -10,7 +10,7 @@ const ContactForm = () => {
     emailjs
       .sendForm(
         process.env.EMAILJS_SERVICE_ID,
-        process.env.REACT_APP_EMAILJS_TEMPLATE_ID,
+        process.env.EMAILJS_TEMPLATE_ID,
         form.current,
         {
           publicKey: process.env.EMAILJS_PUBLIC_KEY,
