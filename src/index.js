72967a1dd002d0c5c8f9c7618d9a76f183875569
MatteoPiselli <pro.matteo.piselli@gmail.com>
2025-02-13 19:25:38 +0100

    Suppression de la configuration de pré-déploiement et retrait de BrowserRouter dans index.js

--- src/index.js
+++ src/index.js
@@ -1,16 +1,12 @@
 import React, { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
 import "./styles/index.css";
 import App from "./App";
-
-const basename =
-  process.env.NODE_ENV === "production" ? "/react-portfolio" : "/";
 
 const root = createRoot(document.getElementById("root"));
 
 root.render(
-  <BrowserRouter basename={basename}>
+  <StrictMode>
     <App />
-  </BrowserRouter>
+  </StrictMode>
 );
