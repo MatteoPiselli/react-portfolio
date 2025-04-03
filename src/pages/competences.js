3e916f9452ed557a0f37bb5567b134440dda78bc
Matteo <pro.matteo.piselli@gmail.com>
2025-04-03 18:49:52 +0200

    Update competences.js
    ajout des nouvelles compétences

--- src/pages/competences.js
+++ src/pages/competences.js
@@ -8,10 +8,7 @@ import html from "../assets/images/competences/html.svg";
 import css from "../assets/images/competences/css.svg";
 import js from "../assets/images/competences/js.svg";
 import github from "../assets/images/competences/github.svg";
-//import java from "../assets/images/competences/java.svg";
-import mySQL from "../assets/images/competences/mySQL.svg";
-//import php from "../assets/images/competences/php.svg";
-//import python from "../assets/images/competences/python.svg";
+import mongoDB from "../assets/images/competences/MongoDB.png";
 import bash from "../assets/images/competences/bash.svg";
 import react from "../assets/images/competences/reactjs.svg";
 import windows from "../assets/images/competences/windows.svg";
@@ -19,8 +16,9 @@ import ubuntu from "../assets/images/competences/ubuntu.svg";
 import macos from "../assets/images/competences/macOS.svg";
 import figma from "../assets/images/competences/figma.png";
 import tailwind from "../assets/images/competences/tailwind.svg";
-//import uml from "../assets/images/competences/uml.svg";
 import node from "../assets/images/competences/Node.js.svg";
+import next from "../assets/images/competences/Next.js.png";
+import express from "../assets/images/competences/Express.png";
 
 const Competences = () => {
   const competencesRef = useRef(null);
@@ -42,7 +40,7 @@ const Competences = () => {
           Mes Compétences / Découvertes
         </h1>
 
-        <div className="ml-[10%] mr-[10%] grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 gap-8">
+        <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 gap-8 ml-[10%] mr-[10%] ">
           <div className="relative transform transition-transform duration-300 hover:scale-105 bg-gray-800 rounded-lg p-5">
             <img src={html} alt="html" className="rounded-lg h-auto" />
             <div className="absolute top-0 right-0 m-2 bg-black bg-opacity-50 rounded-lg px-2 py-1">
@@ -77,6 +75,16 @@ const Competences = () => {
           </div>
 
           <div className="relative transform transition-transform duration-300 hover:scale-105 bg-gray-800 rounded-lg p-5">
+            <img src={tailwind} alt="tailwind css" />
+            <div className="absolute top-0 right-0 m-2 bg-black bg-opacity-50 rounded-lg px-2 py-1">
+              <span className="text-white">Front-End</span>
+            </div>
+            <div className="absolute inset-0 bg-black bg-opacity-75 flex items-center justify-center rounded-lg opacity-0 hover:opacity-100 transition-opacity duration-300">
+              <span className="text-sm text-white">TAILWIND</span>
+            </div>
+          </div>
+
+          <div className="relative transform transition-transform duration-300 hover:scale-105 bg-gray-800 rounded-lg p-5">
             <img src={react} alt="react" />
             <div className="absolute top-0 right-0 m-2 bg-black bg-opacity-50 rounded-lg px-2 py-1">
               <span className="text-white">Front-End</span>
@@ -87,12 +95,12 @@ const Competences = () => {
           </div>
 
           <div className="relative transform transition-transform duration-300 hover:scale-105 bg-gray-800 rounded-lg p-5">
-            <img src={tailwind} alt="tailwind css" />
+            <img src={next} alt="next" />
             <div className="absolute top-0 right-0 m-2 bg-black bg-opacity-50 rounded-lg px-2 py-1">
-              <span className="text-white">Front-End</span>
+              <span className="text-white">Full Stack</span>
             </div>
             <div className="absolute inset-0 bg-black bg-opacity-75 flex items-center justify-center rounded-lg opacity-0 hover:opacity-100 transition-opacity duration-300">
-              <span className="text-sm text-white">TAILWIND</span>
+              <span className="text-sm text-white">NEXT</span>
             </div>
           </div>
 
@@ -106,45 +114,25 @@ const Competences = () => {
             </div>
           </div>
 
-          {/*<div className="relative transform transition-transform duration-300 hover:scale-105 bg-gray-800 rounded-lg p-5">
-            <img src={php} alt="php" />
+          <div className="relative transform transition-transform duration-300 hover:scale-105 bg-gray-800 rounded-lg p-5">
+            <img src={express} alt="express" />
             <div className="absolute top-0 right-0 m-2 bg-black bg-opacity-50 rounded-lg px-2 py-1">
               <span className="text-white">Back-End</span>
             </div>
             <div className="absolute inset-0 bg-black bg-opacity-75 flex items-center justify-center rounded-lg opacity-0 hover:opacity-100 transition-opacity duration-300">
-              <span className="text-sm text-white">PHP</span>
+              <span className="text-sm text-white">EXPRESS</span>
             </div>
-          </div>*/}
-
-          {/*<div className="relative transform transition-transform duration-300 hover:scale-105 bg-gray-800 rounded-lg p-5">
-            <img src={java} alt="java" />
-            <div className="absolute top-0 right-0 m-2 bg-black bg-opacity-50 rounded-lg px-2 py-1">
-              <span className="text-white">Back-End</span>
-            </div>
-            <div className="absolute inset-0 bg-black bg-opacity-75 flex items-center justify-center rounded-lg opacity-0 hover:opacity-100 transition-opacity duration-300">
-              <span className="text-sm text-white">JAVA</span>
-            </div>
-          </div>*/}
+          </div>
 
           <div className="relative transform transition-transform duration-300 hover:scale-105 bg-gray-800 rounded-lg p-5">
-            <img src={mySQL} alt="MySQL" />
+            <img src={mongoDB} alt="MongoDB" />
             <div className="absolute top-0 right-0 m-2 bg-black bg-opacity-50 rounded-lg px-2 py-1">
               <span className="text-white">Base de Donées</span>
             </div>
             <div className="absolute inset-0 bg-black bg-opacity-75 flex items-center justify-center rounded-lg opacity-0 hover:opacity-100 transition-opacity duration-300">
-              <span className="text-sm text-white">MYSQL</span>
+              <span className="text-sm text-white">MongoDB</span>
             </div>
           </div>
-
-          {/*<div className="relative transform transition-transform duration-300 hover:scale-105 bg-gray-800 rounded-lg p-5">
-            <img src={python} alt="python" />
-            <div className="absolute top-0 right-0 m-2 bg-black bg-opacity-50 rounded-lg px-2 py-1">
-              <span className="text-white">Algo</span>
-            </div>
-            <div className="absolute inset-0 bg-black bg-opacity-75 flex items-center justify-center rounded-lg opacity-0 hover:opacity-100 transition-opacity duration-300">
-              <span className="text-sm text-white">PYTHON</span>
-            </div>
-          </div>*/}
 
           <div className="relative transform transition-transform duration-300 hover:scale-105 bg-gray-800 rounded-lg p-5">
             <img src={bash} alt="bash" />
@@ -155,16 +143,6 @@ const Competences = () => {
               <span className="text-sm text-white">BASH</span>
             </div>
           </div>
-
-          {/*<div className="relative transform transition-transform duration-300 hover:scale-105 bg-gray-800 rounded-lg p-5">
-            <img src={uml} alt="uml" />
-            <div className="absolute top-0 right-0 m-2 bg-black bg-opacity-50 rounded-lg px-2 py-1">
-              <span className="text-white">Modélisation</span>
-            </div>
-            <div className="absolute inset-0 bg-black bg-opacity-75 flex items-center justify-center rounded-lg opacity-0 hover:opacity-100 transition-opacity duration-300">
-              <span className="text-sm text-white">UML</span>
-            </div>
-          </div>*/}
 
           <div className="relative transform transition-transform duration-300 hover:scale-105 bg-gray-800 rounded-lg p-5">
             <img src={github} alt="github" />
